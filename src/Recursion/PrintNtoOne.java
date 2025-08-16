22bd200932df828afa1fd4cf9e0056d6d8eb01b1
shubham18 <shubhamadane009@gmail.com>
2025-08-16 23:03:32 +0530

    DSA problem sovled by shubham

--- src/Recursion/PrintNtoOne.java
+++ src/Recursion/PrintNtoOne.java
@@ -2,6 +2,14 @@ package Recursion;
 
 public class PrintNtoOne {
     public static void main(String[] args) {
+          print(10);
+    }
+    public static void print(int i)
+    {
+        if(i<1)
+            return;
 
+        System.out.println(i);
+        print(i-1);
     }
 }
